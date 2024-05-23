ecbc62a8111df5eb56847aaa73f1459f81370562
Evanjalin-K <jojaev510@gmail.com>
2024-05-23 23:33:36 +0530

    Working with array list and Usage of keys

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,25 @@
-const App = ()=>{   //App is a component
-  return <h1>Hello World</h1> 
+const App = () => {
+
+  const friends = [
+    {id:1,name: "Peter",age: 20},
+    {id:2,name: "John",age: 23},
+    {id:3,name: "lia",age: 29},
+    {id:4,name: "Fana",age: 25}
+  ]
+  return (
+    <div>
+      <h1>Friends</h1>
+      <ul>
+      {
+        friends.map(friend=> //call back function
+          <li key={friend.id}>{friend.name} {friend.age}</li>
+        )
+      }
+      </ul>
+    </div>
+  )
 }
 
-export default App;
+export default App
+
+//Using map function we can get dynamic data
