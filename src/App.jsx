2d777f11c44fdb4dcb8c4f16e8bcd96e83db469e
Evanjalin-K <jojaev510@gmail.com>
2024-05-23 19:24:34 +0530

    created multiple components --Hello component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
-const App = ()=>{   //App is a component
-  return <h1>Hello World</h1> 
+const Hello = () => {
+return <h1>Hello,World!</h1>
 }
 
+
+
+const App = ()=>{   //App is a component
+  return (         //multiple h1 can be created inside the () and div tag
+    <div>   
+      <Hello/> 
+      <Hello/> 
+      <Hello/> 
+    </div>
+  )
+}
 export default App;
