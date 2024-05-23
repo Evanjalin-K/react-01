c2e42d4c6beb2986b82fbbf6402bb4b0f39ff8b1
Evanjalin-K <jojaev510@gmail.com>
2024-05-24 00:07:33 +0530

    conditional rendering --using if else

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,11 @@
 const App = () => {
+  const isLoggedin = false;
 
-  const friends = [
-    {id:1,name: "Peter",age: 20},
-    {id:2,name: "John",age: 23},
-    {id:3,name: "lia",age: 29},
-    {id:4,name: "Fana",age: 25}
-  ]
-  const listFriends = [];
-  for (let i =0; i < friends.length; i++){
-    listFriends.push(
-      <li key={friends[i].id}>{friends[i].name} {friends[i].age}</li>
-    )
+  if(isLoggedin){
+    return <h1>Welcome Back!</h1>
   }
-  console.log(listFriends)
-
-  return (
-    <div>
-      <h1>Friends</h1>
-      <ul>
-      {
-      listFriends  
-      }
-      </ul>
-    </div>
-  )
+  else{
+    return <h1>Please Log in</h1>
 }
-
+}
 export default App
-
-//Another method by using for loop - map() is the best way to do.
