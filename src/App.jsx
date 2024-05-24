aa3fae53e179d60f9480fc377923dd7eb71117e0
Evanjalin-K <jojaev510@gmail.com>
2024-05-24 13:38:19 +0530

    increment and decrement count using class this.state

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,34 @@
-import { Component } from "react";
-import Hello from "./components/Hello"
- 
-class App extends Component {
-  render(){
-    return(
-      <div>
-     <Hello
-     name = "John"/>
+import { Component } from 'react';
 
-      </div>
-    )
+export class App extends Component {
+  constructor(props) {
+    super(props);
+    // set the initial state
+    this.state = {
+      counter:0
+    }
   }
+
+
+handleDecrement = () => {
+  this.setState ({
+    counter: this.state.counter -1
+  });
+}
+handleIncrement = () => {
+  this.setState ({
+    counter: this.state.counter +1
+  });
+}
+render() {
+  console.log(this.state)
+return (
+    <div>
+      <p>count: {this.state.counter}</p>
+      <button onClick={this.handleIncrement}>Increment</button>
+      <button onClick={this.handleDecrement}>Decrement</button>
+    </div>
+  )
+}
 }
 export default App;
